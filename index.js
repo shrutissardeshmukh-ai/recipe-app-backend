6def1084d183d0c8c9b03eade41e883e466c2894
shrutissardeshmukh-ai <shrutissardeshmukh@gmail.com>
2025-10-26 11:27:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const authRoutes = require('./routes/auth');
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 // Basic route
 app.get('/', (req, res) => res.send('API running'));
