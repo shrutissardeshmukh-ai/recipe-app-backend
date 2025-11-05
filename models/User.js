26c5963e8d08126e82c0b014569e64c72f76bcc1
shrutissardeshmukh-ai <shrutissardeshmukh@gmail.com>
2025-11-05 12:27:09 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,27 +1,25 @@
+
+
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    lowercase: true,
   },
   password: {
     type: String,
     required: true,
   },
-  favorites: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Recipe',
-    }
-  ],
-}, { timestamps: true });
+  favorites: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Recipe',
+  }],
+});
 
 export default mongoose.model('User', userSchema);
